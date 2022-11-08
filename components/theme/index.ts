5edea2f005a23aaea0fe05b3089315482a6e184f
Ippei Nojima <i-nojima@jfe-systems.com>
2022-11-08 22:15:49 +0900

    themeをメモ化。

--- components/theme/index.ts
+++ components/theme/index.ts
@@ -1,12 +1,13 @@
 import { createTheme } from "@mui/material"
+import { useMemo } from "react"
 
-export const useTheme = () => {
-  const theme = createTheme({
+export const useAooTheme = () => {
+  const theme = useMemo(() => createTheme({
     palette: {
       background: {
         default: '#f8f9fa'
       },
     }
-  })
+  }), [])
   return theme
 }
